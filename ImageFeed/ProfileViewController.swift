dc7fd725e5f22e208ecf18db1cc9de31e4746f60
Олег Аксененко <manikryte@yandex.ru>
2023-03-05 16:12:30 +1000

    sprint_09: Добавлен экран профиля пользователя

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Олег Аксененко on 05.03.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
