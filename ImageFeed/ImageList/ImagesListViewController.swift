ab6be1043a2e2fdd9bb75b5a359f464fea04154f
Олег Аксененко <manikryte@yandex.ru>
2023-03-05 13:34:24 +1000

    sprint_08: Добавлен экран с лентой для просмотра фотографий

---
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ImagesListViewController.swift
+//  ImageFeed
+//
+//  Created by Олег Аксененко on 12.02.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+
+    private let photosName = { (0..<20).map(String.init) }()
+
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.contentInset = UIEdgeInsets(top: 12, left: .zero, bottom: 12, right: .zero)
+    }
+
+
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        photosName.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImageListCell.reuseIdentifier, for: indexPath)
+
+        guard let imageListCell = cell as? ImageListCell else {
+            return UITableViewCell()
+        }
+
+        configCell(for: imageListCell, indexPath: indexPath)
+
+        return imageListCell
+    }
+}
+
+private extension ImagesListViewController {
+    func configCell(for cell: ImageListCell, indexPath: IndexPath) {
+        let image = UIImage(named: photosName[indexPath.row])
+        guard let image else { return }
+
+        let evenIndexPath = indexPath.row % 2 == 0
+        let likeButtonImage = UIImage(named: evenIndexPath ? "likeButtonOn" : "likeButtonOff")
+
+        guard let likeButtonImage else { return }
+
+        cell.photoImage.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        cell.likeButton.setImage(likeButtonImage, for: .normal)
+    }
+}
+
