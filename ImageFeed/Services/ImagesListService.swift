d0fbab78523a5b05ad9785230a2ddd7edf4529a2
Олег Аксененко <aksenenko_oyu@drom.ru>
2023-05-17 02:25:07 +1000

    sprint_13: Добавлена обработка 500 ответа от сервера при загрузке следующей страницы с картинками

--- ImageFeed/Services/ImagesListService.swift
+++ ImageFeed/Services/ImagesListService.swift
@@ -12,6 +12,7 @@ final class ImagesListService {
 
     private (set) var photos: [Photo] = []
     private var lastLoadedPage: Int?
+    private var lastPageLoaded: Bool = false
 
     private let oauth2TokenStorage = OAuth2TokenStorage()
     private let urlSession = URLSession.shared
@@ -23,7 +24,9 @@ final class ImagesListService {
             assertionFailure("Code was called from non-main thread")
         }
 
-        guard fetchImagesTask == nil, let token = oauth2TokenStorage.token else {
+        guard fetchImagesTask == nil,
+              let token = oauth2TokenStorage.token,
+              lastPageLoaded == false else {
             return
         }
 
@@ -44,8 +47,12 @@ final class ImagesListService {
                 photos.append(contentsOf: photosResult.map(Photo.init(photoResult:)))
                 lastLoadedPage = (lastLoadedPage ?? 0) + 1
                 NotificationCenter.default.post(name: Self.DidChangeNotification, object: nil)
-            case .failure:
-                assertionFailure("Something went wrong. Can't get images from UnsplashAPI")
+            case .failure(let error):
+                if let error = error as? NetworkError,
+                   case .httpStatusCode(let statusCode) = error,
+                   statusCode >= 500 {
+                    self.lastPageLoaded = true
+                }
             }
 
             self.fetchImagesTask = nil
